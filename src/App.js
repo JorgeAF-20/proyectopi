2f5d2cefc5fbde3748a055a2b7d587ffb68e6a88
JorgeAF <jorgeaguilarfernandez2003@gmail.com>
2023-05-14 19:27:14 +0200

    añadida ruta

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import '@aws-amplify/ui-react/styles.css';
+import { Container } from '@mui/material';
 import './App.css';
+import Header from './components/Header/Header';
+import Teachers from './views/teacher/Teachers';
+import { useEffect } from 'react';
+import Students from './views/student/Students';
+import Admins from './views/administrator/Admins';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> AcademiaJorge.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function NotFound() {
+  return <>No existe la página</>;
 }
 
-export default App;
+function rol( group ) {
+  let text = ''
+  switch(group) {
+    case 'administradores':
+      text = 'Administrador: ';
+      break;
+    case 'profesores':
+      text = 'Profesor: ';
+      break;
+    case 'alumnos':
+      text = 'Alumno: ';
+      break;
+    default:
+  }
+  return text
+}
+
+function App({ signOut, user }) { 
+  let navigate = useNavigate();
+  useEffect(() => {
+    navigate('/' + user.signInUserSession.accessToken.payload['cognito:groups'][0]);
+    console.log(user);
+  }, [user])
+
+  let group = user.signInUserSession.accessToken.payload['cognito:groups'][0];
+
+  return (
+        <Container>
+          <Header title="AcademiaJorge" username={user.username} signOut={signOut} url={'/' + group} group={rol(group)} />
+          <Routes>
+            <Route exact path='/' />
+            <Route exact path='/administradores/*' element={<Admins user={user.username} />} />
+            <Route exact path='/profesores/*' element={<Teachers user={user.username}/>} />
+            <Route exact path='/alumnos/*' element={<Students user={user.username}/>} />
+            <Route exact path='*' element={<NotFound />} />
+          </Routes>
+        </Container>
+  );
+}
+export default withAuthenticator(App);
